aafb49a514690311edabe0c6851cf46fc945e096
eclipse_user <eclipse_user@xxg.com>
2018-07-16 15:23:54 +0800

    My first commit in eclipse

---
+++ src/main/java/cn/itcast/git/GitTest_1.java
@@ -0,0 +1,9 @@
+package cn.itcast.git;
+
+public class GitTest_1 {
+
+	public GitTest_1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/test_git/GitTest_2.java
@@ -0,0 +1,9 @@
+package test_git;
+
+public class GitTest_2 {
+
+	public GitTest_2() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
